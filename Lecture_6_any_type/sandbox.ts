a8b041cc6ecd32aeaddd8c4cf1625236c81a2882
Dvarkesh12 <dvarkeshg1@gmail.com>
2023-10-17 11:57:07 +0530

    EXPLICITE DATA TYPES

---
+++ Lecture_6_any_type/sandbox.ts
@@ -0,0 +1,17 @@
+// LESSON 6: DYNAMIC (ANY) TYPE
+
+// Resverse moving towards the Javascript
+// UseCase : When data type is not known to variable.
+// You think that you will be changing data type
+
+let age_t: any= 25;
+
+age_t = true;
+console.log(age_t)
+
+age_t = "Hello";
+console.log(age_t)
+
+age_t = {name_t: 'dg'};
+console.log(age_t)
+
